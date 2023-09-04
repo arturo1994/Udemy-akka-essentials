408edc6f43d32cd5681882a43ca19bbf3b393e85
Arturo Carreño Oliveri <arturo.carreno@wizeline.com>
2023-09-04 17:18:38 -0400

    part three GeneralRecap section

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -1,5 +1,7 @@
 package part1recap
 
+import scala.util.Try
+
 object GeneralRecap extends App {
 
   val aCondition: Boolean = false
@@ -82,11 +84,47 @@ object GeneralRecap extends App {
     println("some logs")
   }
 
+  // functional programing
 
+  val incrementer = new Function1[Int, Int] {
+    override def apply(v1: Int): Int = v1 + 1
+  }
 
+  val incremented = incrementer(42) //43
+  // incrementer.apply(42)
 
+  val anonymousIncrementer = (x: Int) => x+1
+  // Int => === Function1[Int, Int]
 
+  // FP is all about working with functions as first-class
+  List(1,2,3).map(incrementer)
+  // map = Higher order function
 
+  //for comprehensions
 
+  val pairs = for{
+    num <- List(1, 2, 3, 4)
+    char <- List("a", "b", "c", "d")
+  } yield num + "-" + char
+
+  // List(1,2,3,4).flapMap(num => List("a", "b", "c", "d").map(char => num + "-" + char))
+  // Seq, Array, List, Vector, Map, Tuples, Sets
+
+  //collections
+  //Option and try
+  val anOption = Some(2)
+  val aTry = Try(
+    throw new RuntimeException
+  )
+
+  //pattern matching
+
+  val bob = Person("bob", 22)
+  val greating = bob match {
+    case Person(n, _) => s"Hi, my name in $n"
+    case _ => "I dont know my name"
+  }
+
+  // ALL THE PATTERNS
 
 }
