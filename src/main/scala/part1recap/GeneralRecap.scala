565599bd84649476776a5951604bc4c3433c14ba
Arturo Carreño Oliveri <arturo.carreno@wizeline.com>
2023-09-04 16:53:20 -0400

    Second part GeneralRecap section

--- src/main/scala/part1recap/GeneralRecap.scala
+++ src/main/scala/part1recap/GeneralRecap.scala
@@ -32,5 +32,61 @@ object GeneralRecap extends App {
     else factrialFunction(n-1, acc*n)
   }
 
+  // OOP
+  class Animal
+  class Dog extends Animal
+  val aDog: Animal = new Dog
+
+  trait Carnivore {
+    def eat(a: Animal): Unit
+  }
+
+
+  class Crocodile extends Animal with Carnivore{
+    override def eat(a: Animal): Unit = println("crunch!!!")
+  }
+
+  // method notations
+
+  val aCroc = new Crocodile
+  aCroc.eat(aDog)
+  aCroc eat aDog
+
+  // anonymus classes
+
+  val aCarnivore = new Carnivore {
+    override def eat(a: Animal): Unit = println("roar!!!")
+  }
+
+  aCarnivore eat aDog
+
+  //generics
+
+  abstract class MyList[+A]
+
+  // companion object
+
+  object MyList
+
+  // case classes
+
+  case class Person(name:String, age:Int) // a lot in this course
+
+  // Exceptions
+
+  val aPotentialFailure = try{
+    throw new RuntimeException("I am innocent, I swear!") // Nothing
+  } catch {
+    case e: Exception => "I caught an exception"
+  } finally {
+    println("some logs")
+  }
+
+
+
+
+
+
+
 
 }
